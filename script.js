9824482231d42307d118955c35e85964831f2562
tarunsai2912 <140894256+tarunsai2912@users.noreply.github.com>
2024-04-10 16:15:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function appendToResult(val) {
-    document.getElementById('result').value += val;
-  }
+  document.getElementById('result').value += val;
+}
   
-  function clearResult() {
-    document.getElementById('result').value = '';
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+function deleteResult(){
+  document.getElementById('result').value = document.getElementById('result').value.slice(0,-1)
+}
+function calculateResult() {
+  var result = document.getElementById('result').value;
+  try {
+    var calculatedResult = eval(result);
+    document.getElementById('result').value = calculatedResult;
+  } catch (error) {
+    alert('Invalid expression');
   }
-  function deleteResult(){
-    document.getElementById('result').value = document.getElementById('result').value.slice(0,-1)
-  }
-  function calculateResult() {
-    var result = document.getElementById('result').value;
-    try {
-      var calculatedResult = eval(result);
-      document.getElementById('result').value = calculatedResult;
-    } catch (error) {
-      alert('Invalid expression');
-    }
-  }
+}
   
