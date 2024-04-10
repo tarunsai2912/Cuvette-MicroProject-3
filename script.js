e88795ec34ef7628a059cafd898124a371b6af5f
tarunsai2912 <140894256+tarunsai2912@users.noreply.github.com>
2024-04-10 23:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function appendToResult(val) {
-  document.getElementById('result').value += val;
+  if (val === '.' && document.getElementById('result').value.includes('.')) {
+    return;
+  }
+  if ('+-*/'.includes(val) && document.getElementById('result').value.slice(-1).match(/[+\-*/]/)) {
+    document.getElementById('result').value = document.getElementById('result').value.slice(0, -1) + val;
+  } 
+  else {
+    document.getElementById('result').value += val;
+  }
 }
-  
+
 function clearResult() {
   document.getElementById('result').value = '';
 }
@@ -12,7 +20,12 @@ function calculateResult() {
   var result = document.getElementById('result').value;
   try {
     var calculatedResult = eval(result);
-    document.getElementById('result').value = calculatedResult;
+    if (calculatedResult.toString().includes('.')){
+      document.getElementById('result').value = calculatedResult.toFixed(2);
+    }
+    else{
+      document.getElementById('result').value = calculatedResult;
+    }
   } catch (error) {
     alert('Invalid expression');
   }
